6a94af0d96024b3db19ed4137680efddbe1c4302
Spencer Tranter <spencertranter95@gmail.com>
2016-09-07 02:04:56 +0000

    Created method that returns an object of the total sales tax for each province

--- advancedPractice.js
+++ advancedPractice.js
@@ -21,3 +21,41 @@ var companySalesData = [
     sales: [ 500, 100 ]
   }
 ];
+
+function calculateSalesTax(sales, taxRates) {
+  var obj = {};
+  for(var sales in companySalesData){
+    var totalRegionSales = 0;
+    var regionSales = companySalesData[sales].sales;
+
+    for(var i = 0; i < regionSales.length; i++){
+      totalRegionSales += regionSales[i];
+    }
+    var regionTax = totalRegionSales * taxRates[companySalesData[sales].province];
+
+    if (!obj.hasOwnProperty(companySalesData[sales].name))  {
+      obj[companySalesData[sales].name] = {};
+      obj[companySalesData[sales].name]["totalSales"] = totalRegionSales;
+      obj[companySalesData[sales].name]["totalTaxes"] = regionTax;
+    } else {
+      obj[companySalesData[sales].name]["totalSales"] += totalRegionSales;
+      obj[companySalesData[sales].name]["totalTaxes"] += regionTax;
+    }
+  }
+  return obj;
+}
+
+var results = calculateSalesTax(companySalesData, taxRates);
+
+/* Expected Results:
+{
+  Telus: {
+    totalSales: 1300
+    totalTaxes: 144
+  },
+  Bombardier: {
+    totalSales: 800,
+    totalTaxes: 40
+  }
+}
+*/
